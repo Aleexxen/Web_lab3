05452da5ce1c901438ff1ce8f5e52d89604454c6
Aleexxen <49920406+Aleexxen@users.noreply.github.com>
2020-04-22 17:46:46 +0300

    Change script3

--- script3.js
+++ script3.js
@@ -15,8 +15,28 @@ const object3 = {
 
 // Реализуйте функцию isEquivalent
 
-const isEquivalent = (obj1, obj2) =>
-  JSON.stringify(obj1) === JSON.stringify(obj2)
+const isEquivalent = (obj1, obj2) =>{
+  if (obj1 === obj2){
+		return true
+	}else if((typeof obj1 === 'object' && obj1 !==null)&&(typeof obj2 === 'object' && obj2 !==null)){
+		if(Object.keys(obj1).length !== Object.keys(obj2).length){
+			return false
+}
+
+for(let el in obj1){
+	if(obj2.hasOwnProperty(el)){
+		if(!isEquivalent(obj1[el],obj2[el])){
+			return false
+		}
+	}else{
+		return false
+	}
+	}
+	return true
+}else{
+	return false
+}
+	}
 
 console.log(isEquivalent(object1, object2)) // true
-console.log(isEquivalent(object1, object3)) // false
+console.log(isEquivalent(object1, object3)) // fals
