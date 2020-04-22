ff34e8c1abe1097f2cc92df5dd3114d23cff6534
Aleexxen <49920406+Aleexxen@users.noreply.github.com>
2020-04-22 16:22:37 +0300

    Change script4

--- script4.js
+++ script4.js
@@ -1,12 +1,11 @@
 function User(name) {
   this.name = name
-  this.getName = () => this.name
-
-  return {
-    getName: this.getName,
-  }
 }
 
+
+User.prototype.getName = function() {
+return this.name;
+}
 // Допишите код здесь, чтобы программа вывела на консоль ожидаемый результат
 
 const me = new User('Rex')
